c42724c023703c6697df2f91a0818477012aa8fe
Florian Eck <it-support@friends-systems.de>
2019-02-25 18:51:11 +0100

    remove root_folder setting

--- lib/easyredmine_office365_connector/base.rb
+++ lib/easyredmine_office365_connector/base.rb
@@ -64,32 +64,12 @@ module EasyredmineOfficeConnector
       @user = user
     end
 
-    def create_root_folder
-      post("/me/contactfolders", { "displayName": EasyredmineOfficeConnector.contact_folder_name }) do |response|
-        begin
-          if JSON.parse(response.body)['id']
-            @user.update_column(
-              :office365_contact_folder_id, JSON.parse(response.body)['id']
-            )
-          end
-        rescue Exception => e
-          # TODO: Add Custom Error Logger for Office
-          binding.pry
-        end
-      end
-    end
-
     def create_or_update_contact(easy_contact)
-      if @user.office365_contact_folder_id.nil?
-        create_root_folder
-        @user.reload
-      end
-
       # decide wether to update or create
       settings = if easy_contact.o365_id_for_user(@user).nil? # => create
-        { url: "/me/contactFolders/#{@user.office365_contact_folder_id}/contacts" , method: :post }
+        { url: "/me/contacts" , method: :post }
       else
-        { url: "/me/contactFolders/#{@user.office365_contact_folder_id}/contacts/#{easy_contact.o365_id_for_user(@user)}" , method: :patch }
+        { url: "/me/contacts/#{easy_contact.o365_id_for_user(@user)}" , method: :patch }
       end
 
       self.send(settings[:method], settings[:url], easy_contact.to_o365_hash) do |response|
